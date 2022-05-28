2984230a2d643b8cf2321686a3f25995e55e6774
Wilson Daniel Machado Toro <wilson.machado@correounivalle.edu.co>
2022-05-28 17:07:07 +0100

    Se entregan requerimientos y script en Python para que funcione el ADC MCP3008 con la Raspberry Pi 4.

---
+++ daq.py
@@ -0,0 +1,26 @@
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import time
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+ 
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+ 
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+ 
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
+
+while True:
+  try:
+    print('ADC Voltage: {:.2f} V'.format(chan.voltage))
+    time.sleep(0.1)
+  except KeyboardInterrupt:
+    print('\nExiting...')
+    break
