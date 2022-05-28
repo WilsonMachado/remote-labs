3e45ecf98c5589fb28aa9c1ab443fc7fa45ea7cc
Wilson Daniel Machado Toro <wilson.machado@correounivalle.edu.co>
2022-05-28 19:03:17 +0100

    Ups, creo que no hice control+S jsajsjas. Hacer caso al commit inmediatamente anterior

--- dac.py
+++ dac.py
@@ -1,22 +1,10 @@
 import time
-import board
-import busio
-
-# Simple demo of setting the output voltage of the MCP4725 DAC.
-# Will alternate setting 0V, 1/2VDD, and VDD each second.
-# Author: Tony DiCola
-# License: Public Domain
-import time
-
 # Import the MCP4725 module.
 import Adafruit_MCP4725
 
 # Create a DAC instance.
-dac = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1)
+dac = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1) # Tener en cuenta la dirección. Para A0 = GND, addr = 0x60. Para A0 = VCC, addr = 0x62.
 
-# Note you can change the I2C address from its default (0x62), and/or the I2C
-# bus by passing in these optional parameters:
-#dac = Adafruit_MCP4725.MCP4725(address=0x49, busnum=1)
 
 # Loop forever alternating through different voltage outputs.
 print('Press Ctrl-C to quit...')
