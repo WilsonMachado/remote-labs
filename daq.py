f0daf8ecb2c4824388f0072ac0547728e4e1fce3
Wilson Daniel Machado Toro <wilson.machado@correounivalle.edu.co>
2022-05-28 22:59:01 +0100

    Se modifica el archivo daq.py para que realice una operación sencilla con los datos que vienen del ADC y coloque el resultado en el DAC. Cabe mencionar que los datos potvenientes del ADC se procesan en un hilo

--- daq.py
+++ daq.py
@@ -1,26 +1,45 @@
+# Librerías del sistema
+
 import busio
 import digitalio
 import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+import threading
 import time
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
- 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
- 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
- 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+# Librerías para los perifericos
+
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import Adafruit_MCP4725
+
+#--------------------------------------------------------------------------------------------------------#
+
+# Objeto para el ADC
+adc = MCP.MCP3008(busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI), digitalio.DigitalInOut(board.D5))
+channel_0 = AnalogIn(adc, MCP.P0) # Canal 0 del ADC
+
+# Objeto para el DAC
+dac = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1) # Tener en cuenta la dirección. Para A0 = GND, addr = 0x60. Para A0 = VCC, addr = 0x62.
+
+# Hilo para la adqusición de los datos
+
+def theard_adc_get_data():
+    while True:
+        dac.set_voltage(int(4*(channel_0.voltage*1023)/3.3))
+        print(channel_0.voltage)
+        time.sleep(0.1)
+
+theard_adc_get_data_manager = threading.Thread(target=theard_adc_get_data)
+theard_adc_get_data_manager.daemon = True 
+theard_adc_get_data_manager.start()
+
+theard_adc_get_data_manager.join()
+
+print("Pille el multímetro papá...")
 
 while True:
   try:
-    print('ADC Voltage: {:.2f} V'.format(chan.voltage))
-    time.sleep(0.1)
+    pass
   except KeyboardInterrupt:
     print('\nExiting...')
     break
